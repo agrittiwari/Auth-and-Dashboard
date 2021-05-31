ebcce6c3b7191b17cbda60dbf8689c7d8cc145c2
Agrit Tiwari <agrit1408@gmail.com>
2021-05-31 11:03:44 +0530

    building starting structure

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
 const express = require('express');
 const app = express()
+const path= require('path')
+
+//parsing data
+app.use(express.json({extended: false}))
+
+
+app.get('/', (req, res) =>
+{
+    res.sendFile(path.resolve(__dirname,'index.html'))
+})
 
 
 const PORT = process.env.PORT || 5000
